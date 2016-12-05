6297f0290aa09528f97aa4ae926693f62d4f59fc
Huynh <alexhuynher@gmail.com>
2016-12-04 16:17:38 -0800

    Moved all functions and variables into objects

--- public/client.js
+++ public/client.js
@@ -8,43 +8,52 @@ var ttt = {
   
   checkWin: function(){
     if(//Horizontal Wins
-     player1.hasOwnProperty(0) && player1.hasOwnProperty(1) && player1.hasOwnProperty(2) ||
-     player1.hasOwnProperty(3) && player1.hasOwnProperty(4) && player1.hasOwnProperty(5) ||
-     player1.hasOwnProperty(6) && player1.hasOwnProperty(7) && player1.hasOwnProperty(8) ||
+     this.player1.hasOwnProperty(0) && this.player1.hasOwnProperty(1) && this.player1.hasOwnProperty(2) ||
+     this.player1.hasOwnProperty(3) && this.player1.hasOwnProperty(4) && this.player1.hasOwnProperty(5) ||
+     this.player1.hasOwnProperty(6) && this.player1.hasOwnProperty(7) && this.player1.hasOwnProperty(8) ||
      //Vertical Wins
-     player1.hasOwnProperty(0) && player1.hasOwnProperty(3) && player1.hasOwnProperty(6) ||
-     player1.hasOwnProperty(1) && player1.hasOwnProperty(4) && player1.hasOwnProperty(7) ||
-     player1.hasOwnProperty(2) && player1.hasOwnProperty(5) && player1.hasOwnProperty(8) ||
+     this.player1.hasOwnProperty(0) && this.player1.hasOwnProperty(3) && this.player1.hasOwnProperty(6) ||
+     this.player1.hasOwnProperty(1) && this.player1.hasOwnProperty(4) && this.player1.hasOwnProperty(7) ||
+     this.player1.hasOwnProperty(2) && this.player1.hasOwnProperty(5) && this.player1.hasOwnProperty(8) ||
      //Diagnol wins
-     player1.hasOwnProperty(0) && player1.hasOwnProperty(4) && player1.hasOwnProperty(8) ||
-     player1.hasOwnProperty(2) && player1.hasOwnProperty(4) && player1.hasOwnProperty(6)
+     this.player1.hasOwnProperty(0) && this.player1.hasOwnProperty(4) && this.player1.hasOwnProperty(8) ||
+     this.player1.hasOwnProperty(2) && this.player1.hasOwnProperty(4) && this.player1.hasOwnProperty(6)
     )
     
     {
     console.log("Player 1 wins!");
     console.log("Play again!");
-    
+    this.player1 = {};
+    this.player2 = {};
     }
     else if(//Horizontal Wins
-     player2.hasOwnProperty(0) && player1.hasOwnProperty(1) && player1.hasOwnProperty(2) ||
-     player2.hasOwnProperty(3) && player1.hasOwnProperty(4) && player1.hasOwnProperty(5) ||
-     player2.hasOwnProperty(6) && player1.hasOwnProperty(7) && player1.hasOwnProperty(8) ||
+     this.player2.hasOwnProperty(0) && this.player1.hasOwnProperty(1) && this.player1.hasOwnProperty(2) ||
+     this.player2.hasOwnProperty(3) && this.player1.hasOwnProperty(4) && this.player1.hasOwnProperty(5) ||
+     this.player2.hasOwnProperty(6) && this.player1.hasOwnProperty(7) && this.player1.hasOwnProperty(8) ||
      //Vertical Wins
-     player2.hasOwnProperty(0) && player1.hasOwnProperty(3) && player1.hasOwnProperty(6) ||
-     player2.hasOwnProperty(1) && player1.hasOwnProperty(4) && player1.hasOwnProperty(7) ||
-     player2.hasOwnProperty(2) && player1.hasOwnProperty(5) && player1.hasOwnProperty(8) ||
+     this.player2.hasOwnProperty(0) && this.player1.hasOwnProperty(3) && this.player1.hasOwnProperty(6) ||
+     this.player2.hasOwnProperty(1) && this.player1.hasOwnProperty(4) && this.player1.hasOwnProperty(7) ||
+     this.player2.hasOwnProperty(2) && this.player1.hasOwnProperty(5) && this.player1.hasOwnProperty(8) ||
      //Diagnol wins
-     player2.hasOwnProperty(0) && player1.hasOwnProperty(4) && player1.hasOwnProperty(8) ||
-     player2.hasOwnProperty(2) && player1.hasOwnProperty(4) && player1.hasOwnProperty(6)
+     this.player2.hasOwnProperty(0) && this.player1.hasOwnProperty(4) && this.player1.hasOwnProperty(8) ||
+     this.player2.hasOwnProperty(2) && this.player1.hasOwnProperty(4) && this.player1.hasOwnProperty(6)
     )
     {
       console.log("Player 2 wins!");
       console.log("Play again!");
+      this.player1 = {};
+      this.player2 = {};
     }
   },
   
-  move: function(player,square){
-    player[square] = true;
-    checkWin();
+  move: function(player, square){
+    if(player === 'player1'){
+      this.player1[square] = true;
+    }else if(player === 'player2'){
+      this.player2[square] = true;
+    }else{
+      console.log("invalid command");
+    }
+    this.checkWin();
   }
 };
